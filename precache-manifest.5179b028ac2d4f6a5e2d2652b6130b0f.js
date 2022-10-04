2186e75ba02bd06bab6ff3757f71ca42ea2dec58
sangboking <cowg001@naver.com>
2022-10-04 17:20:14 +0900

    Updates

---
+++ precache-manifest.5179b028ac2d4f6a5e2d2652b6130b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4a1cfb2124a395b4765",
+    "url": "/react-movie-ex/static/css/main.b4a050c6.chunk.css"
+  },
+  {
+    "revision": "c4a1cfb2124a395b4765",
+    "url": "/react-movie-ex/static/js/main.25b165da.chunk.js"
+  },
+  {
+    "revision": "a3a855276c31133aac7e",
+    "url": "/react-movie-ex/static/js/runtime~main.42f26076.js"
+  },
+  {
+    "revision": "756da94f05b39d931bae",
+    "url": "/react-movie-ex/static/css/2.caec1f9d.chunk.css"
+  },
+  {
+    "revision": "756da94f05b39d931bae",
+    "url": "/react-movie-ex/static/js/2.4c27e5a4.chunk.js"
+  },
+  {
+    "revision": "52dc544762154a31163d6ef344d92f27",
+    "url": "/react-movie-ex/index.html"
+  }
+];
