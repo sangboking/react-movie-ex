46bfc12eebf72672c6530aaaa28482ca8e66aad3
sangboking <cowg001@naver.com>
2022-10-04 17:09:23 +0900

    feat : swiper 라이브러리 적용

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,13 @@ import axios from "../api/axios";
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal/index";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+import 'swiper/components/navigation/navigation.min.css'
+import 'swiper/components/pagination/pagination.min.css'
 
 export default function Row({title, id, fetchURL, isLargeRow}) {
   const [movies, setMovies] = useState([]);
@@ -10,7 +17,7 @@ export default function Row({title, id, fetchURL, isLargeRow}) {
 
   useEffect(() => {
     fetchMovieData();
-  }, []) 
+  }, []);
 
   const fetchMovieData = async() => {
     const request = await axios.get(fetchURL);
@@ -25,44 +32,52 @@ export default function Row({title, id, fetchURL, isLargeRow}) {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
-
+      <Swiper 
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} 
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, 
+            slidesPerGroup: 6, 
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation  
+        pagination={{ clickable: true }} 
+      >
         <div id={id} className="row__posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`https://image.tmdb.org/t/p/original/${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            } `}
-            alt={movie.name}
-            onClick={() => handleClick(movie)}
-          />
-        ))}
+          {movies.map((movie) => (
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                style={{ padding: "25px 0" }}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                } `}
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
+          ))}
         </div>
-        <div className="slider__arrow-right">
-          <span className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >{">"}</span>
-        </div>
-      </div>
-
+      </Swiper>
+       
       {
         modalOpen && (
           <MovieModal {...movieSelected} setModalOpen={setModalOpen}/>
-        )
-      }
+      )}
     </section>
- );
+  );
 }
--- src/hooks/useOnclickOutside.js
+++ src/hooks/useOnclickOutside.js
@@ -10,10 +10,8 @@ export const useOnclickOutside = (ref, handler) => {
     };
 
     document.addEventListener("mousedown", listener);
-    document.addEventListener("touchstart", listener);
     return () => {
       document.removeEventListener("mousedown", listener);
-      document.removeEventListener("touchstart", listener);
     };
   }, [ref, handler]);
 };
